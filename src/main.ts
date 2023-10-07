4e2a62c42683e6d6a1e76a2f42ca53cdb4ab8d20
voskanvl <voskanvl@gmail.com>
2023-10-07 23:34:48 +0300

    animate text

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.sass"
 import "@splidejs/splide/css/default"
 import Splide from "@splidejs/splide"
+import type { SlideComponent } from "@splidejs/splide"
 
 const slider = new Splide("#slider", {
     type: "loop",
@@ -8,6 +9,15 @@ const slider = new Splide("#slider", {
 })
 slider.mount()
 
-const hasid = document.querySelector<HTMLVideoElement>("#hasid"),
-    loading = document.querySelector<HTMLElement>(".loading")
-hasid && hasid.addEventListener("loadeddata", () => loading?.setAttribute("hidden", "hidden"))
+// const hasid = document.querySelector<HTMLVideoElement>("#hasid"),
+//     loading = document.querySelector<HTMLElement>(".loading")
+// hasid && hasid.addEventListener("loadeddata", () => loading?.setAttribute("hidden", "hidden"))
+
+slider.on("active", (event: SlideComponent) => {
+    const text = event.slide.querySelector(".text")
+    text && text.classList.add("show")
+})
+slider.on("inactive", (event: SlideComponent) => {
+    const text = event.slide.querySelector(".text")
+    text && text.classList.remove("show")
+})
